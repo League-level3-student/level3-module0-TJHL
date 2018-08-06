177f6abc077b8a5e68161e7eeff6c9b8730914c1
TJHL <league@iMac-2.attlocal.net>
2018-08-05 17:22:08 -0700

    Start processing thing

--- src/_00_IntroToArrays/_02_FindTheHiddenButton.java
+++ src/_00_IntroToArrays/_02_FindTheHiddenButton.java
@@ -2,6 +2,7 @@ package _00_IntroToArrays;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -15,6 +16,7 @@ import org.omg.Messaging.SyncScopeHelper;
 public class _02_FindTheHiddenButton implements ActionListener {
 	String tempToInt ;
 	int numOfButtons=0;
+	int hiddenButtons;
 	JFrame window;
 	JPanel panel;
 
@@ -42,15 +44,13 @@ public class _02_FindTheHiddenButton implements ActionListener {
 		window = new JFrame("Find the Button");
 		panel = new JPanel();
 		window.add(panel);
-		
-	//	window.setSize(1920, 970);
-		
-		
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		
 		tempToInt=JOptionPane.showInputDialog("Enter a positive number");
 		numOfButtons= Integer.parseInt(tempToInt);
+	
+		
 		if(numOfButtons>673) {
 			numOfButtons=672;
 		}
@@ -62,30 +62,46 @@ public class _02_FindTheHiddenButton implements ActionListener {
 			 buttonFormation[i].addActionListener(this);
 			 panel.add(buttonFormation[i]);
 		}
-		//window.pack();
+		
 		
 		window.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		window.setVisible(true);
 		
+		
 		// 12. Give the user the instructions for the game.
-
+	
+		JOptionPane.showMessageDialog(null, "START?");
 		// 13. initialize the hiddenButton variable to a random number less than the int
 		// created in step 3
-
+		Random randomButton = new Random();
+		hiddenButton=randomButton.nextInt(numOfButtons);
+		System.out.println(hiddenButton);
+		buttonFormation[hiddenButton].setText("HELP");
 		// 14. Set the text of the JButton located at hiddenButton to read "ME"
-
+		try {
+			Thread.sleep(100);
+		} catch (InterruptedException e) {
+			
+			e.printStackTrace();
+		}
 		// 15. Use Thread.sleep(100); to pause the program.
-
+		buttonFormation[hiddenButton].setText("");
 		// 16. Set the text of the JButton located at hiddenButton to be blank.
-
+		
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		JButton buttonClicked = (JButton) e.getSource();
-
+		if(e.getSource()==buttonFormation[hiddenButton]) {
+			JOptionPane.showMessageDialog(null,"Congragul-*****-lations, you won");
+		}
+		if(e.getSource()!=buttonFormation[hiddenButton]) {
+			JOptionPane.showMessageDialog(null,"Oh gee darn, you suck");
+		}
 		// 17. if the hiddenButton is clicked, tell the user that they win.
 
 		// 18. else tell them to try again
+		
 	}
 }
